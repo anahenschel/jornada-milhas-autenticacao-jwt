6ce133e89351c64b92b4027f45179d8c36c325ad
Ana Henschel <Henschel.Ana@senior.com.br>
2025-02-14 10:33:42 -0300

    Adicionando novas alterações no projeto

--- 3213-jornada-milhas/src/app/core/interceptor/autenticacao.interceptor.ts
+++ 3213-jornada-milhas/src/app/core/interceptor/autenticacao.interceptor.ts
@@ -1,4 +1,3 @@
-import { TokenService } from 'src/app/core/services/token.service';
 import { Injectable } from '@angular/core';
 import {
   HttpRequest,
@@ -7,21 +6,22 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TokenService } from '../services/token.service';
 
 @Injectable()
 export class AutenticacaoInterceptor implements HttpInterceptor {
 
-  constructor(private TokenService: TokenService) {}
+  constructor(private tokenService: TokenService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-   if(this.TokenService.possuiToken()) {
-    const token = this.TokenService.retornarToken()
-    request = request.clone({
-      setHeaders: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-   }
+    if(this.tokenService.possuiToken()) {
+      const token = this.tokenService.retornarToken();
+      request = request.clone({
+        setHeaders: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+    }
     return next.handle(request);
   }
 }
